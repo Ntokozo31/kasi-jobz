e73c96e1f4a2d20b96f541f03624995ecfdae0b8
Ntokozo31 <dubentokozo45@yahoo.com>
2025-06-29 15:49:34 +0200

    feat(users): Add users router
    Save new users in database, used save method
    Add users router to get all users,  used get method

--- kasi_jobz/backend/routes/userRoutes.js
+++ kasi_jobz/backend/routes/userRoutes.js
@@ -1,12 +1,32 @@
 // Import express.
 const express = require('express');
 
-// Initialize router
+// Initialize router.
 const router = express.Router();
 
-router.post('/api/users', (req, res) => {
-    res.status(200).json({ message: "User created successfully"})
-})
+// Import User model.
+const User = require('../models/user');
+
+// Create and save new user
+router.post('/api/users', async (req, res) => {
+    try {
+        const user = new User(req.body)
+        await user.save();
+        res.status(200).json({ message: "User created successfully", user})
+    } catch (error) {
+        res.status(500).json({ message: "Error saving user", error: error.message})
+    }
+});
+
+// Get users.
+router.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find().select('-password');
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching users", error: error.message})
+    }
+});
 
 
 // Export 
