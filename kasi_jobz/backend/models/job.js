5d79dc8b4ee584fa6f70d138234badca3c567696
Ntokozo31 <dubentokozo45@yahoo.com>
2025-06-29 16:41:03 +0200

    feat: Define job Schema
    Define job Schema
    Create job Model

---
+++ kasi_jobz/backend/models/job.js
@@ -0,0 +1,18 @@
+// Import mongoose.
+const mongoose = require('mongoose');
+
+// Define Schema.
+const jobSchema = new mongoose.Schema({
+    title: { type: String, required: true},
+    company: { type: String, required: true},
+    location: { type: String, required: true},
+    description: { type: String, required: true},
+    salary: String,
+    createdAt: { type: Date, default: Date.now}
+});
+
+// Create user Model.
+const Job = mongoose.model('Job', jobSchema, 'kasi_jobz');
+
+// Export.
+module.exports = Job;
